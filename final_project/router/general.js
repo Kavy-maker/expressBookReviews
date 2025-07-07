5e78afdf560b97fe87d90808438a2c5f6f086c2e
Kavy-maker <kavitayvonne@gmail.com>
2025-07-07 09:50:46 -0400

    Initial commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,8 +73,24 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn; // Get the ISBN from the web address
+
+    // Check if the book exists
+    if (!books[isbn]) {
+        return res.status(404).json({ message: `Book with ISBN: ${isbn} not found` });
+    }
+
+    // Get the reviews for the book
+    const reviews = books[isbn].reviews;
+
+    // Check if reviews exist and are not empty
+    if (!reviews || Object.keys(reviews).length === 0) {
+        return res.status(404).json({ message: `No reviews found for ISBN: ${isbn}` });
+    }
+
+    // If we reach here, reviews exist and are not empty
+    return res.status(200).json({ book_reviews: reviews });
 });
+
 
 module.exports.general = public_users;
