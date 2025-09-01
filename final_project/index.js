3279df2e201ece1a02500ccf617e0dbbc82fca86
Kavy-maker <kavitayvonne@gmail.com>
2025-09-01 09:31:08 -0400

    Initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -17,23 +17,23 @@ app.use(session({
 
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-    //Write the authenication mechanism here
-    // Check if user is logged in and has valid access token
-    if (req.session.authorization) {
-        let token = req.session.authorization['accessToken'];
-        // Verify JWT token
-        jwt.verify(token, "access", (err, user) => {
-            if (!err) {
-                req.user = user;
-                next(); // Proceed to the next middleware
-            } else {
-                return res.status(403).json({ message: "User not authenticated" });
-            }
-        });
-    } else {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
         return res.status(403).json({ message: "User not logged in" });
     }
+
+    jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+            req.user = user;
+            next();
+        } else {
+            return res.status(403).json({ message: "User not authenticated" });
+        }
+    });
 });
+
 
 const PORT = 5000;
 
